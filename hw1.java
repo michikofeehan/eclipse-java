d98854d44cdb0eb4525cca186bde51e0f43c04e4
Michiko <michiko.feehan@gmail.com>
2018-03-21 01:35:33 -0400

    I added some printing stuff

---
+++ hw1.java
@@ -0,0 +1,10 @@
+
+public class hw1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("What's up homie!!!");
+		System.out.println("welcome back! It is March 21, 2018! when I write this");
+	}
+
+}
